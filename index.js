f8432eeb01c26b43128803db4e4a4753361b02da
Jay Oh <jay.oh@kanopy.com>
2020-12-29 20:52:21 -0800

    adding responsive design for mountain

--- index.js
+++ index.js
@@ -5,10 +5,38 @@ let scroll = window.pageYOffset;
 
 
 document.addEventListener('scroll', () => {
+    console.log("scroll: ", scroll);
     var offset = window.pageYOffset;
     scroll = offset;
     layer1.style.width = (100 + scroll / 5) + '%';
+    // console.log("layer1 width: ", layer1.style.width);
     layer2.style.width = (100 + scroll / 5) + '%';
     layer2.style.left = scroll / 50 + '%';
     zoomText.style.bottom = scroll / 10 + '%';
+    // if (screen.width > 1440) {
+    //     layer1.style.width = (100 + scroll / 5) + '%';
+    //     // console.log("layer1 width: ", layer1.style.width);
+    //     layer2.style.width = (100 + scroll / 5) + '%';
+    //     layer2.style.left = scroll / 50 + '%';
+    //     zoomText.style.bottom = scroll / 10 + '%';
+    // }
+    // else if (screen.width <= 1440) {
+    //     layer1.style.width = (100 + scroll / 15) + '%';
+    //     layer2.style.width = (100 + scroll / 15) + '%';
+    //     layer2.style.left = scroll / 100 + '%';
+    //     zoomText.style.bottom = scroll / 5 + '%';
+    // }
+    // else if (screen.width <= 1270) {
+    //     layer1.style.width = (100 + scroll / 15) + '%';
+    //     layer2.style.width = (100 + scroll / 15) + '%';
+    //     layer2.style.left = scroll / 100 + '%';
+    //     zoomText.style.bottom = scroll / 5 + '%';
+    // }
 })
+
+if (screen.width <= 1440) {
+    layer1.style.width = 110 + '%';
+    layer2.style.width = 110 + '%';
+    // layer2.style.left = scroll / 100 + '%';
+    zoomText.style.bottom = 0 + '%';
+}
